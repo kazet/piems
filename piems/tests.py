c47c7ce7ce8635307f8037f6e3f7f98ae43abf16
kazet <krzysztof.zajac2@gmail.com>
2021-01-12 21:42:39 +0100

    Ordering bugfix (#6)

--- piems/tests.py
+++ piems/tests.py
@@ -18,6 +18,10 @@ class SICTestCase(TestCase):
     def test_subtraction(self):
         self.assertEqual(evaluate("1h 2m - 2h 3m"), HourMinuteTimeInterval(hours=Decimal(-1), minutes=Decimal(-1)))
 
+    def test_addition_and_subtraction_ordering(self):
+        self.assertEqual(evaluate("1h + 2h - 1h"), HourMinuteTimeInterval(hours=Decimal(2)))
+        self.assertEqual(evaluate("1h - 1h + 2h"), HourMinuteTimeInterval(hours=Decimal(2)))
+
     def test_const_multiplication(self):
         self.assertEqual(evaluate("3 * 1h 2m"), HourMinuteTimeInterval(hours=Decimal(3), minutes=Decimal(6)))
         self.assertEqual(evaluate("1h 2m * 3"), HourMinuteTimeInterval(hours=Decimal(3), minutes=Decimal(6)))
