d20b2ba67e4feb9caa893cfb3ebb4572ac9cc15d
kazet <krzysztof.zajac2@gmail.com>
2021-01-12 21:40:28 +0100

    Cleanups and refactors (#5)
    * Cleanups and refactors
    
    * Flake8

--- piems/hourminutetime.py
+++ piems/hourminutetime.py
@@ -1,9 +1,12 @@
 from decimal import Decimal
 
 
-class HMTimeInterval:
+class HourMinuteTimeInterval:
     def __init__(self, hours: Decimal = 0, minutes: Decimal = 0):
         self._minutes = hours * 60 + minutes
+
+    def __repr__(self):
+        return self.__str__()
 
     def __str__(self):
         result = ''
@@ -19,31 +22,31 @@ class HMTimeInterval:
         return result
 
     def __add__(self, obj):
-        assert isinstance(obj, HMTimeInterval)
-        return HMTimeInterval(minutes=self._minutes + obj._minutes)
+        assert isinstance(obj, HourMinuteTimeInterval)
+        return HourMinuteTimeInterval(minutes=self._minutes + obj._minutes)
 
     def __mul__(self, obj):
         assert isinstance(obj, Decimal)
-        return HMTimeInterval(minutes=self._minutes * obj)
+        return HourMinuteTimeInterval(minutes=self._minutes * obj)
 
     def __truediv__(self, obj):
         assert isinstance(obj, Decimal)
-        return HMTimeInterval(minutes=self._minutes / obj)
+        return HourMinuteTimeInterval(minutes=self._minutes / obj)
 
     def __rmul__(self, obj):
         return self.__mul__(obj)
 
     def __sub__(self, obj):
-        assert isinstance(obj, HMTimeInterval)
-        return HMTimeInterval(minutes=self._minutes - obj._minutes)
+        assert isinstance(obj, HourMinuteTimeInterval)
+        return HourMinuteTimeInterval(minutes=self._minutes - obj._minutes)
 
     def __eq__(self, other):
-        if not isinstance(other, HMTimeInterval):
+        if not isinstance(other, HourMinuteTimeInterval):
             return False
         return self._minutes == other._minutes
 
 
-class HMTime:
+class HourMinuteTime:
     def __init__(self, hour: Decimal = 0, minute: Decimal = 0):
         self._hour = hour
         self._minute = minute
@@ -51,9 +54,9 @@ class HMTime:
     def as_minutes_from_midnight(self) -> Decimal:
         return self._hour * 60 + self._minute
 
-    def distance_until(self, obj: "HMTime"):
+    def distance_until(self, obj: "HourMinuteTime"):
         if obj.as_minutes_from_midnight() > self.as_minutes_from_midnight():
-            return HMTimeInterval(minutes=obj.as_minutes_from_midnight() - self.as_minutes_from_midnight())
+            return HourMinuteTimeInterval(minutes=obj.as_minutes_from_midnight() - self.as_minutes_from_midnight())
         else:
-            return HMTimeInterval(
+            return HourMinuteTimeInterval(
                 minutes=Decimal(24 * 60) - self.as_minutes_from_midnight() + obj.as_minutes_from_midnight())
